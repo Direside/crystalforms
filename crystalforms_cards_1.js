e0300af63843f46488a6c2303770fad6633c2748
Shane Gearon <shane.gearon@gmail.com>
2016-02-14 15:03:27 -0800

    Initial files from tablet.

---
+++ crystalforms_cards_1.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var cards = [
+    {
+        name: "Powza",
+        colour: "R",
+        numbers: [1, 5, 6],
+        abilities: [
+            {
+                name: "Slash",
+                text: "Shatter 1."
+            },
+            {
+                name: "Sword and Board",
+                text: "Shatter 1, Shield 1."
+            }
+        ]
+    },
+    {
+        name: "Skit",
+        colour: "R",
+        numbers: [1, 5, 6],
+        abilities: [
+            {
+                name: "Slash",
+                text: "Shatter 1."
+            },
+            {
+                name: "Sword and Board",
+                text: "Shatter 1, Shield 1."
+            }
+        ]
+    }
+]
+
